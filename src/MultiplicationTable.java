880343df7ff91606cf5aaa1690d45324f00f4dd9
foxrw1 <78601276+foxrw1@users.noreply.github.com>
2021-10-13 08:06:46 -0400

    Add files via upload

---
+++ src/MultiplicationTable.java
@@ -0,0 +1,15 @@
+public class MultiplicationTable
+{
+    public static void main(String[] args)
+    {
+        // Call makeMultiplicationTable 
+       makeMultiplicationTable();
+    }
+    
+    // Makes a multiplcation table
+    public static void makeMultiplicationTable()
+    {
+    	//Write your code here
+    	
+    }
+}
